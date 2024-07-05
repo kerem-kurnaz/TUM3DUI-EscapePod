e190a18b15a596805e366bb6de61cdb65b96db8d
keremkz <kurnazkerem@sabanciuniv.edu>
2024-07-05 03:10:08 +0200

    Add button press

---
+++ Assets/_Core/Scripts/SelectionAndManipulation/Pressable.cs
@@ -0,0 +1,81 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace _Core.Scripts.SelectionAndManipulation
+{
+    public class Pressable : MonoBehaviour
+    {
+        public Action<bool> OnPress;
+        
+        public Transform PressableTransform => _pressableTransform;
+        
+        [SerializeField] private float pressMoveFactor = 0.05f; // Scale factor when the button is pressed
+        [SerializeField] private float pressDuration = 0.1f;    // Duration of the press animation
+        [SerializeField] private float releaseDuration = 0.1f;  // Duration of the release animation
+        [SerializeField] private Ease pressEase = Ease.OutQuad; // Easing function for press animation
+        [SerializeField] private Ease releaseEase = Ease.OutBounce; // Easing function for release animation
+        
+        private Selectable _selectable;
+        private Transform _pressableTransform;
+        private bool _readyToPress = false;
+        private bool _isActive = false;
+        
+        private void Awake()
+        {
+            _pressableTransform = transform.parent.parent;
+            
+            _selectable = _pressableTransform.GetComponentInChildren<Selectable>();
+        }
+
+        private void Start()
+        {
+            _selectable.OnSelect += ReadyToPress;
+            _selectable.OnDeselect += NotReadyToPress;
+        }
+
+        private void Update()
+        {
+            if (_readyToPress && Input.GetKeyDown(KeyCode.E))
+            {
+                if (_isActive)
+                {
+                    _isActive = false;
+                }
+                else
+                {
+                    _isActive = true;
+                }
+                
+                Press(_isActive);
+            }
+        }
+
+        private void ReadyToPress()
+        {
+            _readyToPress = true;
+        }
+
+        private void NotReadyToPress()
+        {
+            _readyToPress = false;
+        }
+
+        private void Press(bool activeState)
+        {
+            OnPress?.Invoke(_isActive);
+            
+            var originalPosition = _pressableTransform.localPosition;
+            var pressedPosition = originalPosition + Vector3.down * pressMoveFactor;
+
+            // Animate the move down (press effect)
+            _pressableTransform.DOLocalMove(pressedPosition, pressDuration).SetEase(pressEase)
+                .OnComplete(() =>
+                {
+                    // Animate the move back to the original position (release effect)
+                    _pressableTransform.DOLocalMove(originalPosition, releaseDuration).SetEase(releaseEase);
+                });
+        }
+    }
+}

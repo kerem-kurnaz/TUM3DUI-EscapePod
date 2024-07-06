8fd2c9b53c9887855d66885d2115f5a8dfe1d644
keremkz <kurnazkerem@sabanciuniv.edu>
2024-07-06 14:57:21 +0200

    Add crate sounds

--- Assets/_Core/Scripts/Level/CrateOpener.cs
+++ Assets/_Core/Scripts/Level/CrateOpener.cs
@@ -7,16 +7,21 @@ namespace _Core.Scripts.Level
 {
     public class CrateOpener : MonoBehaviour
     {
+        [SerializeField] private AudioClip openSound;
+        [SerializeField] private AudioClip closeSound;
+        
         [SerializeField] private Transform crateHead;
         
         [SerializeField] private float duration = 1.0f; 
         [SerializeField] private Ease rotationEase = Ease.OutQuad;
         
         private Pressable _pressable;
+        private AudioSource _audioSource;
 
         private void Awake()
         {
             _pressable = GetComponentInChildren<Pressable>();
+            _audioSource = GetComponent<AudioSource>();
         }
 
         private void Start()
@@ -44,11 +49,15 @@ namespace _Core.Scripts.Level
         private void OpenCrate()
         {
             crateHead.DOLocalRotate(new Vector3(-85, 0, 180), duration).SetEase(rotationEase);
+            _audioSource.Stop();
+            _audioSource.PlayOneShot(openSound);
         }
         
         private void CloseCrate()
         {
             crateHead.DOLocalRotate(new Vector3(0, 0, 180), duration).SetEase(rotationEase);
+            _audioSource.Stop();
+            _audioSource.PlayOneShot(closeSound);
         }
     }
 }
